d90fcb2c600de13d5f5132a202323083c3af9159
Alonso Perez <nuevocorreo17@gmail.com>
2019-02-12 18:33:24 -0500

    commit

---
+++ application/modules/dashboard/controllers/Dashboard_controller.php
@@ -0,0 +1,19 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard_controller extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();	
+	}
+
+	
+
+	public function index()
+	{
+	
+	}
+}
+?>
--- application/modules/login/controllers/Login_controller.php
+++ application/modules/login/controllers/Login_controller.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_controller extends MY_Controller
 {
-	public function __construct()
+/*	public function __construct()
 	{
 		parent::__construct();	
-	}
+	}*/
 
 	private function chekarlogin()
 	{
