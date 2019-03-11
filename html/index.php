2e2f854951e7b79586f9314d2f53eab3900d8d38
Alonso Perez <nuevocorreo17@gmail.com>
2019-03-11 04:13:23 -0500

    html

---
+++ html/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+	 	<meta charset="utf-8">
+		<meta name="title" content="DepaExpress">
+		<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam">
+		<meta name="keywords" content="depa, express, departamentos">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">		
+		<title>DepaExpress</title>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<header class="cabecera">
+			<div class="container">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				  <a class="navbar-brand" href="#"><img src="assets/img/logo.svg" class="logo"></a>
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+				  </button>
+				  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav">
+						<li class="nav-item"><a class="nav-link"  href="#">REGÍSTRATE</a></li>
+						<li class="nav-item item-login"><a class="nav-link" href="#">INGRESA<i class="fas fa-user"></i></a></li>
+					</ul>
+				    <ul class="navbar-nav mr-auto">
+				      <li class="nav-item">
+				        <a class="nav-link" href="venta.php">VENTA</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="alquiler.php">ALQUILER</a>
+				      </li>
+				      <li class="nav-item dropdown">
+				        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				          DESARROLLOS INMOBILIARIOS
+				        </a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				          <a class="dropdown-item" href="#">Action</a>
+				          <a class="dropdown-item" href="#">Another action</a>
+				          <a class="dropdown-item" href="#">Something else here</a>
+				        </div>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="agentes.php" >AGENTES</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link publicar" href="publicar.php" >PUBLICAR</a>
+				      </li>      
+				    </ul>
+				  </div>
+				</nav>
+			</div>
+		</header>
+		<main>
+
+		</main>
+		<footer class="piesdepagina">
+			<div class="fondo-azul">
+				<div class="container">
+					<div class="row">
+						<div class="col-12 col-lg-3 widget">
+							<a href="index.php" class="logo-inferior">
+								<img src="assets/img/logo.svg" class="logo">
+							</a>
+							<a href="tel:+51900-0007">(01) 900-0007 </a>
+							<p>Lunes a Viernes de 9 am a 7 pm</p>
+							<p>Sábado de 9 am a 1 pm</p>
+
+						</div>
+						<div class="col-12 col-lg-3 widget">
+							<a href="quienesomos.php">Quiénes somos</a>
+							<a href="mapadesitio.php">Mapa de Sitio</a>
+							<a href="venta.php">Vender</a>
+							<a href="alquiler.php">Alquilar</a>
+						</div>
+						<div class="col-12 col-lg-3 widget">
+							<a href="proyectos.php">Proyectos</a>
+							<a href="inmobiliarias.php">Inmobiliarias & Constructores</a>
+							<a href="Agentes.php">Agentes</a>
+							<a href="preguntasfrecuentes.php">Preguntas Frencuentes</a>
+						</div>	
+						<div class="col-12 col-lg-3 widget">
+							<a href="http.//www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+							<a href="http.//www.instagram.com"><i class="fab fa-instagram"></i></a>
+						</div>																		
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						Copyright depa express <?php echo date('Y'); ?>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<script>
+			//var URL = '<?php echo base_url();?>';
+		</script>
+		<script src="assets/js/jquery.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>	
+		<script src="assets/js/funciones.js"></script>
+	</body>
+</html>
